7838d2b6b222549b37b71c6ad2803c4537a2ec0d
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-06-05 14:50:28 +0200

    Add user input of 6 numbers with basic parsing

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import random
 
 lotto_numbers = random.sample(range(1,50), 6)
 print("Wylosowane liczby lotto to: ", lotto_numbers)
+
+user_input = input("Podaj sześć różnych liczb z zakresu 1-49, oddzielając je spacją: ")
+user_numbers = [int(num) for num in user_input.split()]
